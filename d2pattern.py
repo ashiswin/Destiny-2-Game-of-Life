c06b139e7e5dfe8d4fd7d061f3e4ba52454329f6
Isaac Ashwin <ashiswin@gmail.com>
2020-10-20 14:57:53 +0800

    first commit

---
+++ d2pattern.py
@@ -0,0 +1,24 @@
+from PIL import Image
+from gameoflife import GameOfLife
+import numpy as np
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+im = Image.open("destiny pattern.png")
+pix = im.load()
+
+state = []
+for i in range(0, im.size[0], 3):
+    state.append([])
+    for j in range(0, im.size[1], 3):
+        if pix[i, j] > 0:
+            state[i // 3].append(1)
+        else:
+            state[i // 3].append(0)
+
+im.close()
+
+board = GameOfLife(np.asarray(state, dtype=int))
+for i in range(8):
+    board.saveBoard(f"{i}.tiff")
+    board = board.getNextBoard()
---
+++ gameoflife.py
@@ -0,0 +1,41 @@
+import numpy as np
+from skimage import io
+
+DIRECTIONS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+class GameOfLife:
+
+    def __init__(self, board: np.ndarray):
+        self.board = board
+    
+    def getNextBoard(self):
+        newBoard = np.ndarray(self.board.shape, dtype=int)
+
+        for i in range(self.board.shape[0]):
+            for j in range(self.board.shape[1]):
+                neighbours = self.countNeighbours((i, j))
+
+                if neighbours < 2:
+                    newBoard[i, j] = 0
+                elif neighbours == 2 and self.board[i, j] == 1:
+                    newBoard[i, j] = 1
+                elif neighbours == 3:
+                    newBoard[i, j] = 1
+                elif neighbours > 3:
+                    newBoard[i, j] = 0
+
+        return GameOfLife(newBoard)
+
+    def countNeighbours(self, pos: tuple) -> int:
+        neighbours = 0
+
+        for d in DIRECTIONS:
+            if pos[0] + d[0] < 0 or pos[1] + d[1] < 0 or pos[0] + d[0] >= self.board.shape[0] or pos[1] + d[1] >= self.board.shape[1]:
+                continue
+
+            neighbours += self.board[pos[0] + d[0], pos[1] + d[1]]
+
+        return neighbours
+    
+    def saveBoard(self, filename: str):
+        io.imsave(filename, self.board.transpose().astype(np.uint8) * 255)
